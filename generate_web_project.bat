46d8816510ce178a48dd3d0e43c5a794ccdc9e04
elujambio <eugeniolujambio@gmail.com>
2016-02-16 20:08:51 -0600

    Added barebone stucture for site

---
+++ generate_web_project.bat
@@ -0,0 +1,15 @@
+mkdir project
+cd project
+mkdir css
+mkdir js
+mkdir img
+mkdir fnt
+touch index.html
+touch thanks.html
+cd css
+touch application.css
+cd ../
+cd js
+touch application.js
+cd ../
+
